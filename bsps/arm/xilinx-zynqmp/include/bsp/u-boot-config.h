730e47b2b4d1613936460fffdae4c0426944b609
Young Nielson <ynielson@windhoverlabs.com>
2022-05-02 14:51:21 -0500

    Initial running build on Xilinx Zynqmp Ultra96 R5 board

---
+++ bsps/arm/xilinx-zynqmp/include/bsp/u-boot-config.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2010 embedded brains GmbH.  All rights reserved.
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef LIBBSP_ARM_XILINX_ZYNQMP_R5_U_BOOT_CONFIG_H
+#define LIBBSP_ARM_XILINX_ZYNQMP_R5_U_BOOT_CONFIG_H
+
+/*
+#define CONFIG_HAS_ETH1
+*/
+
+#endif /* LIBBSP_ARM_XILINX_ZYNQMP_R5_U_BOOT_CONFIG_H */
